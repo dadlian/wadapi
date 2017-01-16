eba9e0915ffe1bf06ef8c2f6987f6ca980698e1a
Sven James <sveninem@gmail.com>
2017-01-16 16:27:36 -0400

    Completed v1 of the Profiler and Profiling API

--- profiler/scripts/clean_entities.php
+++ profiler/scripts/clean_entities.php
@@ -5,8 +5,7 @@
         require_once(dirname(__FILE__)."/../../includes.inc");
 
 	$objectIDs = Array();
-	foreach(DatabaseAdministrator::execute("SELECT A.id as object FROM ObjectStatistic AS A JOIN (SELECT api,endpoint,MAX(modified) AS latest FROM ObjectStatistic ".
-						"GROUP BY api,endpoint) AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified < B.latest") as $row){
+	foreach(DatabaseAdministrator::execute("SELECT B.id as object FROM EndpointStatistic AS A JOIN ObjectStatistic AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified > B.modified") as $row){
 		$objectIDs[] = $row["object"];			
 	}
 	
@@ -15,13 +14,30 @@
 	}
 
 	$queryIDs = Array();
-	foreach(DatabaseAdministrator::execute("SELECT A.id AS query FROM QueryStatistic AS A JOIN (SELECT api,endpoint,MAX(modified) AS latest FROM QueryStatistic ".
-						"GROUP BY api,endpoint) AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified < B.latest") as $row){
+	foreach(DatabaseAdministrator::execute("SELECT B.id as query FROM EndpointStatistic AS A JOIN QueryStatistic AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified > B.modified") as $row){
 		$queryIDs[] = $row["query"];
 	}
 	
 	if($queryIDs){
 		DatabaseAdministrator::execute("DELETE FROM QueryStatistic WHERE id IN (".join(",",$queryIDs).")");
+	}
+
+	$callIDs = Array();
+	foreach(DatabaseAdministrator::execute("SELECT B.id as externalCall FROM EndpointStatistic AS A JOIN CallStatistic AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified > B.modified") as $row){
+		$callIDs[] = $row["externalCall"];
+	}
+	
+	if($callIDs){
+		DatabaseAdministrator::execute("DELETE FROM CallStatistic WHERE id IN (".join(",",$callIDs).")");
+	}
+
+	$customIDs = Array();
+	foreach(DatabaseAdministrator::execute("SELECT B.id as customKey FROM EndpointStatistic AS A JOIN CustomStatistic AS B ON A.api = B.api AND A.endpoint = B.endpoint AND A.modified > B.modified") as $row){
+		$customIDs[] = $row["customKey"];
+	}
+	
+	if($customIDs){
+		DatabaseAdministrator::execute("DELETE FROM CustomStatistic WHERE id IN (".join(",",$customIDs).")");
 	}
 						
 	Janitor::cleanup();
