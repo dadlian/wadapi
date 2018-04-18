66caa1da6b92b7bf6ab3948d6e4aa6b1dfd869a7
Sven James <sven@kycsar.com>
2018-04-18 16:43:15 -0400

    Changed WadapiRequest to use CURL. Decrease profiler data archiving to 7 days

--- profiler/scripts/purge_data.php
+++ profiler/scripts/purge_data.php
@@ -4,7 +4,7 @@
         $settings = dirname(__FILE__)."/../settings.xml";
         require_once(dirname(__FILE__)."/../../includes.inc");
 
-	$cutoff = date("Y-m-d h:i:s",strtotime("-28days"));
+	$cutoff = date("Y-m-d h:i:s",strtotime("-7days"));
 	DatabaseAdministrator::execute("DELETE FROM Resource WHERE id IN (SELECT id FROM EndpointStatistic) AND FROM_UNIXTIME(modified) < '$cutoff'");
 	DatabaseAdministrator::execute("DELETE FROM EndpointStatistic WHERE FROM_UNIXTIME(modified) < '$cutoff'");
 	DatabaseAdministrator::execute("DELETE FROM CallStatistic WHERE FROM_UNIXTIME(modified) < '$cutoff'");
