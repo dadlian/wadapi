a91284ca72b2c7f9ce8cdd0a821f14fff01fb0fd
Sven James <sven@kycsar.com>
2018-08-22 21:26:08 -0400

    Replaced @Object reserved word annotation

--- toolbox/wadadli_annotations.php
+++ toolbox/wadadli_annotations.php
@@ -1,27 +1,27 @@
-<?php	
+<?php
 	abstract class WadadliAnnotation extends Annotation{
 		public $required;
 	}
-	
-	class Object extends WadadliAnnotation{
+
+	class WadapiObject extends WadadliAnnotation{
 		public $class;
 	}
-	
+
 	abstract class DefaultedAnnotation extends WadadliAnnotation{
 		public $default;
 	}
-	
+
 	class Boolean extends DefaultedAnnotation {}
-	
+
 	abstract class ValuedAnnotation extends DefaultedAnnotation{
 		public $values;
 	}
-	
+
 	abstract class RangedAnnotation extends ValuedAnnotation{
 		public $min;
 		public $max;
 	}
-	
+
 	class WadapiString extends RangedAnnotation {}
 	class URL extends WadapiString {}
 	class Email extends WadapiString {}
@@ -33,11 +33,11 @@
 		public $width;
 	}
 	class Text extends WadapiString {}
-	
+
 	class Integer extends RangedAnnotation {}
 	class WadapiFloat extends RangedAnnotation {}
 	class Monetary extends RangedAnnotation {}
-	
+
 	class Collection extends RangedAnnotation{
 		public $type;
 	}
