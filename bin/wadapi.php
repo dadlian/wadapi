f9055117b881074cd6ae798c97d71f99aca56674
Sven James <sven@kycsar.com>
2018-08-30 21:34:23 -0400

    Reorganised project as standalone package

--- bin/wadapi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$settings = dirname(__FILE__)."/api/settings.xml";
-        require_once(dirname(__FILE__)."/../../core/includes.inc");
-        
-	//Dispatch Request
-        $activeDispatcher = SettingsManager::getSetting("install","dispatcher");
-        $activeDispatcher::dispatchRequest();
-?>
--- profiler/wadapi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$settings = dirname(__FILE__)."/settings.xml";
-        require_once(dirname(__FILE__)."/../includes.inc");
-	
-	//Dispatch Request
-        $activeDispatcher = SettingsManager::getSetting("install","dispatcher");
-        $activeDispatcher::dispatchRequest();
-?>
