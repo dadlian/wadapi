ad4d81dcc261497a2067b8214614ba5967da26fb
dadlian <sveninem@gmail.com>
2023-03-16 21:01:15 +0000

    Entrypoint updates

--- docker/bin/wadapi_entrypoint.sh
+++ docker/bin/wadapi_entrypoint.sh
@@ -2,4 +2,12 @@
 
 SUBSTR=s~\${BASE_URL}~$BASE_URL~g
 sed -i $SUBSTR /var/www/html/conf/nginx/nginx-site.conf
-/start.sh
+
+# Start nGinx and redirect output to log
+/start.sh > /wadapi.log 2>&1 &
+
+# Start RabbitMQ Subscriber script
+php /var/www/html/project/messaging.php > /wadapi.log 2>&1 &
+
+# Send output log to docker log
+tail -f /wadapi.log
