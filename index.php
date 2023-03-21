d9aa86eedaeb137e056300c59e4db1740043d5c3
dadlian <sveninem@gmail.com>
2023-03-21 23:55:46 +0000

    New Project Structure

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   use Wadapi\Utility\FileUtility;
   use Wadapi\Routing\Dispatcher;
 
+  define('SCRIPT_START', microtime(true));
   define('PROJECT_PATH', dirname(__FILE__));
-	define('SCRIPT_START', microtime(true));
+  define('CONFIG', PROJECT_PATH."/conf");
+  define('MAPPINGS', PROJECT_PATH."/mappings");
 
 	//Include User Created Files
-	FileUtility::require_all(PROJECT_PATH."/controller");
-	FileUtility::require_all(PROJECT_PATH."/model");
+	FileUtility::require_all(PROJECT_PATH."/src/domain");
+  FileUtility::require_all(PROJECT_PATH."/src/endpoints");
 
   Dispatcher::dispatchRequest();
 ?>
--- messaging.php
+++ messaging.php
@@ -4,12 +4,15 @@
   use Wadapi\Utility\FileUtility;
   use Wadapi\Messaging\SubscriptionManager;
 
-  define('PROJECT_PATH', dirname(__FILE__));
 	define('SCRIPT_START', microtime(true));
+  define('PROJECT_PATH', dirname(__FILE__));
+  define('CONFIG', PROJECT_PATH."/conf");
+  define('MAPPINGS', PROJECT_PATH."/mappings");
 
 	//Include User Created Files
-	FileUtility::require_all(PROJECT_PATH."/controller");
-	FileUtility::require_all(PROJECT_PATH."/model");
+	FileUtility::require_all(PROJECT_PATH."/src/domain");
+  FileUtility::require_all(PROJECT_PATH."/src/services");
+  FileUtility::require_all(PROJECT_PATH."/src/subscribers");
 
   SubscriptionManager::manage();
 ?>
